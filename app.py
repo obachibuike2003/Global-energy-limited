7647581ae0f487b905be30d4e97938289288c44a
Chibuike Obadiegwu <chibuikeobadiegwu@gmail.com>
2026-07-09 10:59:01 +0100

    Move secrets to .env, load via python-dotenv

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ import random, string
 from datetime import datetime, timedelta
 from apscheduler.schedulers.background import BackgroundScheduler
 import atexit
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def generate_ref_code():
     return ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
@@ -25,20 +29,19 @@ def generate_ref_code():
 # App config
 # -------------------
 app = Flask(__name__)
-app.secret_key = "CHANGE_THIS_TO_A_LONG_RANDOM_STRING"
+app.secret_key = os.getenv("SECRET_KEY")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
-app.config["MAIL_USERNAME"] = "globalenergylimted@gmail.com"
-app.config["MAIL_PASSWORD"] = "sgqc npwv qaru fgyr"
-app.config["MAIL_DEFAULT_SENDER"] = "globalenergylimted@gmail.com"
+app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
+app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
+app.config["MAIL_DEFAULT_SENDER"] = os.getenv("MAIL_DEFAULT_SENDER")
 
 mail = Mail(app)
 
 db = SQLAlchemy(app)
-import os
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
